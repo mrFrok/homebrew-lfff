72bfc5ad8a30d35ba053d1fd66099b1a06d2511b
mrFrok <egoroffzakhar@yandex.ru>
2026-05-14 02:41:25 +0300

    v2.0.2

--- Formula/lfff.rb
+++ Formula/lfff.rb
@@ -1,29 +1,28 @@
 class Lfff < Formula
   desc "Free, open-source firmware flasher for Android A/B devices via fastboot"
   homepage "https://github.com/mrFrok/LibreFastbootFirmwareFlasher"
-  version "2.0.1"
-  revision 1
+  version "2.0.2"
   license "GPL-3.0-only"
 
   on_macos do
     on_arm do
       resource "cli" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-macos-aarch64.tar.gz"
-        sha256 "e90821827837d9e85a8eec6f18e70657348ce275777917f32cd4ad2ef84d7c93"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-macos-aarch64.tar.gz"
+        sha256 "cb519542dfbdfb01f1f16ef5f79e3540b3d53a05d89add6c07bd1526c1e27ff1"
       end
       resource "gui" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-gui-macos-aarch64.tar.gz"
-        sha256 "56b31da13150aabaf3b88479a730a0893a15b626b9bed80289f14be9c0d4b625"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-gui-macos-aarch64.tar.gz"
+        sha256 "b67561934c3a887c082e3cbbeb2ba55eddd7c8432c5611de104a4c1cde51a16a"
       end
     end
     on_intel do
       resource "cli" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-macos-x86_64.tar.gz"
-        sha256 "92345f59cffb9f0cede73823d9d4c88113072734bf5d9b1504ebbb6295a7b396"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-macos-x86_64.tar.gz"
+        sha256 "e4219d2ccfd249e222fb887e41bdef5a955674a9c1370ab74f7955aeb76b5018"
       end
       resource "gui" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-gui-macos-x86_64.tar.gz"
-        sha256 "cff5f4971ba4e6939ebe5a55535b646ddf247ac78a079e30cda7279aaee680fb"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-gui-macos-x86_64.tar.gz"
+        sha256 "521838d095822d737a600b5e29680f3b5d462194a50df1e5b8c3f14b8139ab36"
       end
     end
   end
@@ -31,22 +30,22 @@ class Lfff < Formula
   on_linux do
     on_arm do
       resource "cli" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-linux-aarch64.tar.gz"
-        sha256 "b9c1c53c01db72ca6da04202ddf7ed99eb9220dfe927497fa8656f09cd5a483c"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-linux-aarch64.tar.gz"
+        sha256 "d401bdc8ec2734f55fbd8d225544dbbd289218720070dca2875babac1f79806f"
       end
       resource "gui" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-gui-linux-aarch64.tar.gz"
-        sha256 "900c9fd3fa50e40110271dcd285c4edba5b3e0168faeefcd3c2f2b33a00866aa"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-gui-linux-aarch64.tar.gz"
+        sha256 "0c976e4dc3d89181715b95f0c1e7eeeaa88707d6c5c91dd6248ac04c7f575227"
       end
     end
     on_intel do
       resource "cli" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-linux-x86_64.tar.gz"
-        sha256 "a5757090d5a88a8115666607a028dec18cffc68e0a87fe29b53cdaad0967f652"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-linux-x86_64.tar.gz"
+        sha256 "9a86698f81a885d3990db5df132a6ce89bac44aa606819bfae463646bc0a0fe1"
       end
       resource "gui" do
-        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.1/lfff-gui-linux-x86_64.tar.gz"
-        sha256 "45841fc073b07df3847198390bcecfd80a1e00f0a7de4b294f0652e92c0e3fa7"
+        url "https://github.com/mrFrok/LibreFastbootFirmwareFlasher/releases/download/v2.0.2/lfff-gui-linux-x86_64.tar.gz"
+        sha256 "7878201d8576140fa0e32417964090619d57bf75f618cae5d094ab69f58f9322"
       end
     end
   end
